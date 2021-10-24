96c920e2d06a610a568bd17ede77772fdc2db4bf
wangdong <20004604@qq.com>
2021-10-24 17:59:05 +0800

    添加埋点

--- src/page/home/app.js
+++ src/page/home/app.js
@@ -24,5 +24,62 @@ new Vue({
     el: '#app',
     router,
     template: '<App/>',
-    components: { App }
+    components: {App}
 });
+
+
+function getExplore () {
+    const Sys = {};
+    const ua = navigator.userAgent.toLowerCase();
+    let s;
+
+    // eslint-disable-next-line
+    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? Sys.ie = s[1] :
+        (s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
+            (s = ua.match(/edge\/([\d.]+)/)) ? Sys.edge = s[1] :
+                (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
+                    (s = ua.match(/(?:opera|opr).([\d.]+)/)) ? Sys.opera = s[1] :
+                        (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
+                            (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
+
+    // 根据关系进行判断
+    if (Sys.ie) {
+        return ('IE: ' + Sys.ie);
+    }
+    if (Sys.edge) {
+        return ('EDGE: ' + Sys.edge);
+    }
+    if (Sys.firefox) {
+        return ('Firefox: ' + Sys.firefox);
+    }
+    if (Sys.chrome) {
+        return ('Chrome: ' + Sys.chrome);
+    }
+    if (Sys.opera) {
+        return ('Opera: ' + Sys.opera);
+    }
+    if (Sys.safari) {
+        return ('Safari: ' + Sys.safari);
+    }
+    return 'Unkonwn';
+}
+
+const getEnv = () => {
+    let env;
+    if ([ 'localhost', '127.0.0.1' ].indexOf(window.location.hostname) > -1) {
+        env = 'local';
+    } else {
+        env = window.location.hostname.slice(0, 10);
+    }
+    return env;
+};
+
+function sendUV () {
+    const agent = getExplore();
+    const img = document.createElement('img');
+    const uuid = 'uuid';
+    const env = getEnv();
+    img.src = `http://data.lovelovewall.com/api/useragent?uuid=${uuid}&agent=${agent}&status=visit&env=${env}`;
+}
+
+sendUV();

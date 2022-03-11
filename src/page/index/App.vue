f1e3bc1cff9fc485c9ee1c17cf6224e43d3ba158
wangdong <20004604@qq.com>
2022-03-11 11:12:24 +0800

    更新1.1.7版本

--- src/page/index/App.vue
+++ src/page/index/App.vue
@@ -6,7 +6,7 @@
                  class="el-menu-vertical-demo"
                  @select="selectMenu">
             <el-menu-item-group>
-                <template slot="title">开发指南 v1.1.6</template>
+                <template slot="title">开发指南 v1.1.7</template>
                 <el-menu-item index="/Install">安装</el-menu-item>
             </el-menu-item-group>
 

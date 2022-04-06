7ab48dd3e3ca06a882e1a0d8923b65792dab87c6
wangdong <20004604@qq.com>
2022-04-06 09:11:08 +0800

    update

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -76,12 +76,10 @@ const config = {
         progress: false,
         proxy: {
             '/api': {
-                target: 'http://cf-pc-dev.wti-xa.com:7777',
-                // target: 'https://project-bff-dev.wti-xa.com',
-                // target: 'http://172.16.7.115:8080',
+                target: 'http://lovelovewall.com/wtiformdemo',
                 changeOrigin: true,
                 pathRewrite: {
-                    // '^/api': '',
+                    '^/api': '',
                 },
             },
         },

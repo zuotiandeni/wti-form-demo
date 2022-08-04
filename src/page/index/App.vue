a0197ade4b3aa2695f1388a602d7bad2805a987f
wangdong <20004604@qq.com>
2022-08-04 11:01:09 +0800

    更新版本1.1.13

--- src/page/index/App.vue
+++ src/page/index/App.vue
@@ -6,7 +6,7 @@
                  class="el-menu-vertical-demo"
                  @select="selectMenu">
             <el-menu-item-group>
-                <template slot="title">开发指南 v1.1.12.1</template>
+                <template slot="title">开发指南 v1.1.13</template>
                 <el-menu-item index="/Install">安装</el-menu-item>
             </el-menu-item-group>
 

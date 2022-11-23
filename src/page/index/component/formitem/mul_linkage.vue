851a1b4c71b8cf696eab194a6ff858baeeb9128c
wangdong <20004604@qq.com>
2022-11-23 15:21:23 +0800

    1.2.2.1 更新特殊的必填校验

--- src/page/index/component/formitem/mul_linkage.vue
+++ src/page/index/component/formitem/mul_linkage.vue
@@ -22,6 +22,27 @@
         </el-collapse>
 
         <el-divider/>
+
+        <h3>特殊的必填校验</h3>
+        <p>自定义校验函数，因为返回值是一个数组，所以校验规则使用 ElementUI 默认的必填配置，是不会起作用的。</p>
+        <p>如果要求该三级联动是必填的，那么需要写一个自定义必填校验函数</p>
+        <wti-form ref="form2"
+                  :fields="fields2"
+                  :dynamic-select-option="dynamicSelectOption"/>
+        <div class="submit-line">
+            <el-button type="primary" @click="submit('form2')">提交按钮</el-button>
+            <span class="tips">请查看控制台看提交结果</span>
+        </div>
+        <el-collapse class="collapse">
+            <el-collapse-item>
+                <template slot="title">
+                    <b>点击查看代码</b>
+                </template>
+                <pre v-highlightjs><code class="javascript">{{ code2 }}</code></pre>
+            </el-collapse-item>
+        </el-collapse>
+
+        <el-divider/>
     </div>
 </template>
 
@@ -91,8 +112,10 @@ fields1: [
                         children: [
                             {
                                 key: 'key2',
-                                type: 'input',
-                                label: '必填输入框的label',
+                                type: 'mul-linkage',
+                                label: '这是一个必填失效的三级联动下拉框',
+                                linkLevel: 3,
+                                firstParentKey: '100',
                                 rules: [
                                     {
                                         required: true,
@@ -103,55 +126,32 @@ fields1: [
                                         ]
                                     }
                                 ],
-                            }
-                        ]
-                    }
-                ],
-
-                code2: `<wti-form
-            ref="form2"
-            :fields="fields2"
-            :border-form="false"/>
----
-fields2: [
-    {
-        children: [
-            {
-                key: 'key2',
-                type: 'input',
-                label: '必填输入框的label',
-                rules: [
-                    {
-                        required: true,
-                        message: '请输入',
-                        trigger: [
-                            'blur',
-                            'change'
-                        ]
-                    }
-                ],
-            }
-        ]
-    }
-]`,
-
-
-                fields3: [
-                    {
-                        children: [
+                            },
                             {
                                 key: 'key3',
-                                type: 'input',
-                                label: '最少3个字，最多6个字',
+                                type: 'mul-linkage',
+                                label: '这是一个自定义校验的三级联动下拉框',
+                                linkLevel: 3,
+                                firstParentKey: '100',
                                 rules: [
                                     {
+                                        validator (rule, value, callback) {
+                                            let isEmpty = false;
+                                            value.forEach(item => {
+                                                if (!item) {
+                                                    isEmpty = true;
+                                                }
+                                            });
+                                            if (isEmpty) {
+                                                return callback(new Error('请输入'));
+                                            }
+                                            callback();
+                                        },
+                                        // message: '请输入',
                                         trigger: [
                                             'blur',
-                                            'change'
-                                        ],
-                                        max: 6,
-                                        message: '长度应当处于3到6之间',
-                                        min: 3
+                                            // 'change'
+                                        ]
                                     }
                                 ],
                             }
@@ -159,97 +159,31 @@ fields2: [
                     }
                 ],
 
-                code3: `<wti-form
-            ref="form3"
-            :fields="fields3"
-            :border-form="false"/>
+                code2: `<wti-form ref="form1"
+              :fields="fields1"
+              :dynamic-select-option="dynamicSelectOption"/>
 ---
-fields3: [
+dynamicSelectOption: {
+    // 这是字典接口的 url
+    dictUrl: '/dict',
+    // 异步请求时，请求内容是一个对象或一个数组。
+    // 如果是对象，那么包含一个 key 和一个数组。
+    // 如果是数组，那么只有这个数组。
+    // 数组是所有字典 FormItem 的 parentKey 的集合
+    queryKey: 'search', // 这是请求时那个 key。如果为空，则请求时是一个数组，而不是一个对象
+    parentKey: 'parentKey', // 这是返回结果的 parentKey。意思是，同一个 parentKey 归属于同一个下拉框选项
+    value: 'code', // 这是下拉框选项的值
+    label: 'label' // 这是下拉框选项的 label
+},
+fields1: [
     {
         children: [
             {
-                key: 'key3',
-                type: 'input',
-                label: '最少3个字，最多6个字',
-                rules: [
-                    {
-                        trigger: [
-                            'blur',
-                            'change'
-                        ],
-                        max: 6,
-                        message: '长度应当处于3到6之间',
-                        min: 3
-                    }
-                ],
-            }
-        ]
-    }
-]`,
-
-
-                fields4: [
-                    {
-                        children: [
-                            {
-                                key: 'key4',
-                                type: 'input',
-                                label: '比如第 x 月这样就可以这么用',
-                                prepend: '第',
-                                append: '月',
-                                placeholder: '请输入月份'
-                            }
-                        ]
-                    }
-                ],
-
-                code4: `<wti-form
-            ref="form4"
-            :fields="fields4"
-            :border-form="false"/>
----
-fields4: [
-    {
-        children: [
-            {
-                key: 'key4',
-                type: 'input',
-                label: '比如第 x 月这样就可以这么用',
-                prepend: '第',
-                append: '月',
-                placeholder: '请输入月份'
-            }
-        ]
-    }
-]`,
-
-
-                fields5: [
-                    {
-                        children: [
-                            {
-                                key: 'key5',
-                                type: 'input',
-                                label: '给个默认值吧',
-                                defaultValue: '1234'
-                            }
-                        ]
-                    }
-                ],
-
-                code5: `<wti-form
-            ref="form5"
-            :fields="fields5"
-            :border-form="false"/>
----
-fields5: [
-    {
-        children: [
-            {
-                key: 'key5',
-                type: 'input',
-                label: '给个默认值吧',
-                defaultValue: '1234'
+                key: 'key1',
+                type: 'mul-linkage',
+                label: '这是一个三级联动下拉框',
+                linkLevel: 3,
+                firstParentKey: '100'
             }
         ]
     }
